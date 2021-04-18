4cc2f7209e55ddefeda5898fadd7189cd51570a3
LinuxMeow <dimitrije.petrovic92@gmail.com>
2021-04-19 01:27:47 +0200

    First Commit

---
+++ .bashrc
@@ -0,0 +1,16 @@
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='exa --icons --color always -l -a -G --group-directories-first -g -h'
+
+PS1='[\u@\h \W]\$ '
+
+#eval "$(oh-my-posh --init --shell bash --config ~/.poshthemes/powerline.omp.json)"
+
+eval "$(oh-my-posh --init --shell bash --config ~/.poshthemes/powerline.omp.json)"
+
+neofetch
---
+++ .config/awesome/themes/modtheme/theme.lua
@@ -0,0 +1,124 @@
+---------------------------------------------
+-- Awesome theme which follows xrdb config --
+--   by Yauhen Kirylau                    --
+---------------------------------------------
+
+local theme_assets = require("beautiful.theme_assets")
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
+local xrdb = xresources.get_current_theme()
+local gfs = require("gears.filesystem")
+local themes_path = gfs.get_themes_dir()
+
+-- Colours defined
+local arch_blue = "#1793d1"
+local bg_normal = "#000000"
+local fg_normal = "#1793d1"
+local bg_urgent = "#ff1818"
+local bg_focus = "#1793d1"
+local bg_minimize = "#000000"
+local bg_systray = "#000000"
+local tootlip_fg = "#000000"
+local tootlip_bg = "#1793d1"
+local border_normal = "#193465"
+local border_focus = "#1793d1"
+local layout_bg = "#ffffff"
+
+-- Sizes defined
+local useless_gap = 20
+local border_width = 2
+
+-- inherit default theme
+local theme = dofile(themes_path.."default/theme.lua")
+-- load vector assets' generators for this theme
+
+theme.font          = "sans 8"
+
+theme.bg_normal     = bg_normal
+theme.bg_focus      = bg_focus
+theme.bg_urgent     = bg_urgent
+theme.bg_minimize   = bg_minimize
+theme.bg_systray    = bg_systray
+
+theme.fg_normal     = xrdb.foreground
+theme.fg_focus      = theme.bg_normal
+theme.fg_urgent     = theme.bg_normal
+theme.fg_minimize   = theme.bg_normal
+
+theme.useless_gap   = dpi(useless_gap)
+theme.border_width  = dpi(border_width)
+theme.border_normal = border_normal
+theme.border_focus  = border_focus
+theme.border_marked = xrdb.color10
+
+-- There are other variable sets
+-- overriding the default one when
+-- defined, the sets are:
+-- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
+-- tasklist_[bg|fg]_[focus|urgent]
+-- titlebar_[bg|fg]_[normal|focus]
+-- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- mouse_finder_[color|timeout|animate_timeout|radius|factor]
+-- Example:
+--theme.taglist_bg_focus = "#ff0000"
+
+theme.tooltip_fg = tooltip_fg
+theme.tooltip_bg = arch_blue
+
+-- Variables set for theming the menu:
+-- menu_[bg|fg]_[normal|focus]
+-- menu_[border_color|border_width]
+theme.menu_submenu_icon = themes_path.."default/submenu.png"
+theme.menu_height = dpi(16)
+theme.menu_width  = dpi(100)
+
+-- You can add as many variables as
+-- you wish and access them by using
+-- beautiful.variable in your rc.lua
+--theme.bg_widget = "#cc0000"
+
+-- Recolor Layout icons:
+theme = theme_assets.recolor_layout(theme, layout_bg)
+
+-- Define the icon theme for application icons. If not set then the icons
+-- from /usr/share/icons and /usr/share/icons/hicolor will be used.
+theme.icon_theme = nil
+
+-- Generate Awesome icon:
+theme.awesome_icon = theme_assets.awesome_icon(
+    theme.menu_height, theme.bg_focus, theme.fg_focus
+)
+
+-- Generate taglist squares:
+local taglist_square_size = dpi(4)
+theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
+    taglist_square_size, theme.fg_normal
+)
+theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
+    taglist_square_size, theme.fg_normal
+)
+
+-- Taglist styling:
+theme.taglist_spacing = 10
+
+-- Try to determine if we are running light or dark colorscheme:
+local bg_numberic_value = 0;
+for s in theme.bg_normal:gmatch("[a-fA-F0-9][a-fA-F0-9]") do
+    bg_numberic_value = bg_numberic_value + tonumber("0x"..s);
+end
+local is_dark_bg = (bg_numberic_value < 383)
+
+-- Generate wallpaper:
+local wallpaper_bg = xrdb.color8
+local wallpaper_fg = xrdb.color7
+local wallpaper_alt_fg = xrdb.color12
+if not is_dark_bg then
+    wallpaper_bg, wallpaper_fg = wallpaper_fg, wallpaper_bg
+end
+theme.wallpaper = function(s)
+    return theme_assets.wallpaper(wallpaper_bg, wallpaper_fg, wallpaper_alt_fg, s)
+end
+
+return theme
+
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
